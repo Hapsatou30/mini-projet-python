4c89fd6e2947e6cc7502721b84337fd00873cb6b
Haps <thiamhapstou@gmail.com>
2025-03-17 11:12:48 +0000

    Configurer Django

--- RecetteShare/RecetteShare/settings.py
+++ RecetteShare/RecetteShare/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recettes' #Notre application
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'RecetteShare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'recettes_python',
+        'USER': 'root',
+        'PASSWORD': 'hapsatou',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
