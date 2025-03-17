18ed9096d51a5f703ae01fa37f87d3ce623efc4f
Haps <thiamhapstou@gmail.com>
2025-03-17 11:36:24 +0000

    Définition des Modèles

--- RecetteShare/RecetteShare/settings.py
+++ RecetteShare/RecetteShare/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +85,12 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+#
+# URL de base pour accéder aux fichiers médias
+MEDIA_URL = "/media/"
 
+# Chemin absolu du répertoire où les fichiers médias seront stockés
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- RecetteShare/recettes/models.py
+++ RecetteShare/recettes/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#créer les models
+
+#model Recette
+class Recette(models.Model):
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+    titre = models.CharField(max_length=255)
+    description = models.TextField()
+    ingredients = models.TextField()
+    image = models.ImageField(upload_to='recettes/', blank=True, null=True) 
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
+
+#model Commentaire
+class Commentaire(models.Model):
+    recette = models.ForeignKey(Recette, on_delete=models.CASCADE, related_name='commentaires')
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+    contenu = models.TextField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Commentaire de {self.utilisateur.username} sur {self.recette.titre}"
